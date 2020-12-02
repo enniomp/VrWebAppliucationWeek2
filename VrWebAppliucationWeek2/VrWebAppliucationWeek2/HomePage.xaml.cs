1219bcc5fcee0b2c821e48b0956fdc0c96ff75e2
Ennio Paone <1938258paone@zuyd.nl>
2020-12-02 15:41:26 +0100

    Add project files.

---
+++ VrWebAppliucationWeek2/VrWebAppliucationWeek2/HomePage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VrWebAppliucationWeek2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HomePage : TabbedPage
+    {
+        public HomePage()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_OnClicked(object sender, EventArgs e)
+        {
+            Naviation.PushAsync(new NextPage());
+        }
+        private void MenuItem2_OnClicked(object sender, EventArgs e)
+        {
+            Naviation.PushAsync(new NextPage());
+        }
+    }
+}
---
+++ VrWebAppliucationWeek2/VrWebAppliucationWeek2/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace VrWebAppliucationWeek2
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void ViewButton_OnClicked(object sender, EventArgs e)
+        {
+            bool isUsernameEmpty = string.IsNullOrEmpty(userInvoering.Text);
+            bool isPasswordEmpty = string.IsNullOrEmpty(wwInvoering.Text);
+
+            if (isUsernameEmpty || isPasswordEmpty)
+            {
+                nameView.Text = "Beide velden moeten worden ingevuld.";
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+            }
+        }
+    }
+}
